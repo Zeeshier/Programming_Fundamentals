a836520ea738661f65170c8018ee7c75829af048
Zeeshan Ahmad <110332535+Zeeshier@users.noreply.github.com>
2024-08-05 22:11:44 +0500

    Student Grade Calculator

---
+++ Functions/Student Grade Calculator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+char grade(int m);
+
+int main()
+{
+  int marks;
+  char g;
+  cout << "Enter marks: " << endl;
+  cin >> marks;
+  g = grade(marks);
+  cout << "Your Grade is " << g << endl;
+  return 0;	
+}
+char grade(int m)
+{
+	if(m>80)
+	return 'A';
+	else if(m>60)
+	return 'B';
+	else if(m>40)
+	return 'C';
+	else
+	return 'F';
+}
