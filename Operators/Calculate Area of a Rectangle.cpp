9f83ea566f6a4f4cd71a45050c25851c3f11ce61
Zeeshan Ahmad <110332535+Zeeshier@users.noreply.github.com>
2024-08-06 19:23:27 +0500

    Calculate Area of a Rectangle

---
+++ Operators/Calculate Area of a Rectangle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int length = 5;
+    int width = 3;
+    int area = length * width;
+    cout << "Area of rectangle: " << area << endl;
+    return 0;
+}
