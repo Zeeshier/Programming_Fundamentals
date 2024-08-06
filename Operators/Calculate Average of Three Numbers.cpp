90620ae3127d7f46868335c57cfb9da55ba04e26
Zeeshan Ahmad <110332535+Zeeshier@users.noreply.github.com>
2024-08-06 19:25:56 +0500

    Calculate Average of Three Numbers

---
+++ Operators/Calculate Average of Three Numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    int average = (a + b + c) / 3;
+    cout << "Average: " << average << endl;
+    return 0;
+}
