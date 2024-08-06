8294288ec0845950ad344731cfb4aa4167b507b1
Zeeshan Ahmad <110332535+Zeeshier@users.noreply.github.com>
2024-08-06 19:28:36 +0500

    Determine the Largest of Three Numbers

---
+++ Conditionals/Determine the Largest of Three Numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    if (a > b && a > c) {
+        cout << a << " is the largest" << endl;
+    } else if (b > a && b > c) {
+        cout << b << " is the largest" << endl;
+    } else {
+        cout << c << " is the largest" << endl;
+    }
+    return 0;
+}
